2b6b209cc6fde2d18052c8f26f2b35315c51f6f8
Saski <saski.jokela@gmail.com>
2023-10-14 00:26:39 +0300

    add forms.py inside users app

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class EmployerRegistrationForm(forms.Form):
+    full_name = forms.CharField(max_length=100, label='Full Name')
+    company_name = forms.CharField(max_length=100, label='Company Name')
+    email = forms.EmailField(label='Email Address')
+    contact_number = forms.CharField(max_length=20, label='Contact Number')
+    company_location = forms.CharField(max_length=100, label='Company Location/Address')
+    industry = forms.CharField(max_length=100, label='Industry/Sector')
+    username = forms.CharField(max_length=50, label='Username')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    company_description = forms.CharField(widget=forms.Textarea, label='Company Description or Summary')
