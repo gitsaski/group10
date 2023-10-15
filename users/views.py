91d87fdbe3173dd257da0c7324018d5c5c9e6fbd
Saski <saski.jokela@gmail.com>
2023-10-15 14:43:47 +0300

    Add employer_registration on users views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import EmployerRegistrationForm
+from .models import Employer
 
-# Create your views here.
+def employer_registration(request):
+    if request.method == 'POST':
+        form = EmployerRegistrationForm(request.POST)
+        if form.is_valid():
+            # Create an instance of Employer and save it to the database
+            employer = Employer(
+                full_name=form.cleaned_data['full_name'],
+                company_name=form.cleaned_data['company_name'],
+                email=form.cleaned_data['email'],
+                contact_number=form.cleaned_data['contact_number'],
+                company_location=form.cleaned_data['company_location'],
+                industry=form.cleaned_data['industry'],
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                company_description=form.cleaned_data['company_description']
+            )
+            employer.save()
+            return render(request, 'registration_success.html')  # Replace 'registration_success.html' with your desired success page
+    else:
+        form = EmployerRegistrationForm()
+    return render(request, 'employer_registration.html', {'form': form})
