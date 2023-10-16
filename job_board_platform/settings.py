c44f5333369e9fb90c687cf8811dfb554d193020
Saski <saski.jokela@gmail.com>
2023-10-16 14:26:36 +0300

    Edit urls and settings.py to finalize employer registration

--- job_board_platform/settings.py
+++ job_board_platform/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'job_board_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'users', 'templates', 'users')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
