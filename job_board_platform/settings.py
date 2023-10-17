d29fd1e148c2cb4c15c7175efdde3a4818b52e42
Saski <saski.jokela@gmail.com>
2023-10-17 16:38:35 +0300

    removed unnecessary configurations

--- job_board_platform/settings.py
+++ job_board_platform/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'users',
     'jobs',
 
+    # Third party apps
+    'bootstrap4',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,10 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'users/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
