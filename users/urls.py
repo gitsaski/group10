26bd0c7a5bdb0110f9a2307774d118fd8b6dc73c
JaFaKaKs <janne.makinen@edu.turkuamk.fi>
2023-10-29 14:44:19 +0200

    Update company to users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -12,8 +12,12 @@ urlpatterns = [
     path('employer-registration/', views.employer_registration, name='employer_registration'),
      # Employee registration URL
     path('employee-registration/', views.employee_registration, name='employee_registration'),
+    # Company registration URL
+    path('company-registration/', views.company_registration, name='company_registration'),
 
     path('registration_success_employer/', views.registration_success_employer, name='registration_success_employer'),
     
     path('registration_success_employee/', views.registration_success_employee, name='registration_success_employee'),
+
+    path('registration_success_company/', views.registration_success_company, name='registration_success_company'),
 ]
