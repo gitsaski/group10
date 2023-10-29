97c4fa931898da35afb3f53e5a7c2a234974c722
JaFaKaKs <janne.makinen@edu.turkuamk.fi>
2023-10-29 15:43:27 +0200

    linked Company and Admin

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from .models import Company
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'contact_number', 'created_at')
+    search_fields = ('name', 'address', 'contact_number')
+    list_filter = ('created_at',)
+
+admin.site.register(Company, CompanyAdmin)
