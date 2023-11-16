4d17349b0e6e8f3a958ab0c1d0bd98413c48d28d
JaFaKaKs <janne.makinen@edu.turkuamk.fi>
2023-11-16 16:44:38 +0200

    updated models.py
    emplyee had an attribute "company" for some reason

--- users/models.py
+++ users/models.py
@@ -33,7 +33,6 @@ class Employee(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField()
     contact_number = models.CharField(max_length=20)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.full_name
